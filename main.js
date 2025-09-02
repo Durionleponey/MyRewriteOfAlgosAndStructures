88494eb18339401e3c57a15250be6b48efa0b85d
robin <robin.clarembaux@gmail.com>
2025-09-02 14:51:15 +0200

    bubble sort

--- main.js
+++ main.js
@@ -1,133 +1,46 @@
+let list = [2,1321231244,114,5,4,48,5,65,11,13231];
+
+console.log("Avant tri :", list);
 
 
 
+function bubbleSort(list){
 
-class Node{
-    constructor(data){
-        this.data = data;
-        this.left=null
-        this.right=null
+    let c =0
 
-    }
-}
-
-class bst{
-    constructor(){
-        this.root=null
-    }
-    add(value){
+    while(true){
+        c++
 
 
-        if (!this.root){
-            this.root = new Node(value);
-            return value;
-        }
+        for (let i = 0; i < list.length; i++) {
 
-        let currentNode = this.root;
-
-        while(1){
-
-            if (currentNode.data>value){
+            let temp = 0
 
 
-                if (!currentNode.left){
-                    currentNode.left = new Node(value);
-                    return value;
-                }
+            if (list[i] > list[i+1]){
 
-                currentNode = currentNode.left;
-            }else{
-
-                if (!currentNode.right){
-                    currentNode.right = new Node(value);
-                    return value;
-                }
-
-                currentNode = currentNode.right;
+                temp = list[i];
+                list[i] = list[i+1];
+                list[i+1] = temp;
 
             }
-        }
-    }
-
-    remove(value){//not 100% opti and clean and with no edge case but still
-
-        let currentNode = this.root;
-
-        let currentNodeParent = null
-
-        let nodeExtermeLeftParent = null;
-
-        while(true){
-
-            if (currentNode.data === value){
-
-                //console.log("target is found");
-                let target = currentNode;
-
-                //find the closest number to the exterm left i guess
-
-                while(currentNode.left){
-
-                    nodeExtermeLeftParent = currentNode;
-                    currentNode = currentNode.left;
-
-                }
-
-                //change links between target and currentNode
-
-                try{
-                    nodeExtermeLeftParent.left = null;
-
-
-                }catch(e){}
-
-                currentNode.left = target.left;
-                currentNode.right = target.right;
-
-
-                if (currentNodeParent.left === target){
-                    currentNodeParent.left = null;
-                }
-
-                if (currentNodeParent.right === target){
-                    currentNodeParent.right = null;
-                }
-
-                return target;
-
-
-            }else{
-                //searching the target
-
-                currentNodeParent = currentNode;
-
-                if (currentNode.data > value){
-                    currentNode= currentNode.left;
-                }else{
-                    currentNode = currentNode.right;
-                }
-            }
 
 
         }
+
+        if (c> list.length){break}
+
+
+
     }
+
+
+
 }
 
-let robin =new bst();
 
-robin.add(5)
+bubbleSort(list);
 
-robin.add(4)
+console.log("après tri :", list);
 
-robin.add(3)
 
-robin.add(5.5)
-
-robin.add(4.5)
-
-robin.add(6)
-robin.add(9)
-console.log(JSON.stringify(robin, null, 2));
-console.log("REMOVING 3---------------------------------");
-robin.remove(3)
-console.log(JSON.stringify(robin, null, 2));
