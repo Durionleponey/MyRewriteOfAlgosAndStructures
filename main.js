50ca19fc3c4a7679ec2784a7dedd5e6d1f45e431
robin <robin.clarembaux@gmail.com>
2026-06-24 14:12:42 +0200

    fibo with DP

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 function fib(n) {
-    //console.log(n)
-    if (n < 2) return n;
 
-    return fib(n-1)+fib(n-2)
+    let cache = {}
+
+    return fib2(n)
+
+    function fib2(n){
+
+        if (cache[n]){
+            return cache[n]
+        }else{
+            //console.log(n)
+            if (n < 2) return n;
+
+            cache[n] = fib2(n-1)+fib2(n-2)
+
+            return cache[n]
+
+        }
+
+
+    }
+
 
 }
 
